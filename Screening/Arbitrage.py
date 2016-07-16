f9131adfdb7ccd71fbbf36015ab907fe4159a134
spin13 <aaa@aaa>
2016-07-16 12:43:34 +0900

    a

--- Screening/Arbitrage.py
+++ Screening/Arbitrage.py
@@ -1,9 +1,43 @@
 #-*- coding:utf-8 -*-
 
-
+import math
 
 def calc_total_changed(changed_list):
     ret = 100.0
     for i in changed_list:
         ret = ret + (100.0*(i/100.0))
+    return ret
+
+
+def calc_standard_deviation(data_list):
+    avg = 0.0
+    sum = 0.0
+    for i in data_list:
+        sum += i
+    avg = sum/len(data_list)
+
+    dist = 0.0
+    # distribution
+    for i in data_list:
+        dist += ((i - avg) * (i - avg))
+    return math.sqrt(dist)
+
+
+# datalist expected DICTIONARY OBJECT
+# //TODO
+# currently return 2 symbols
+# it will be some symbols to arbitrage
+def check_arbitrage_chance(data_list, allowed, arb_chance):
+    sorted_data = []
+    for k, v in sorted(data_list.items(), key=lambda x:x[1]):
+        sorted_data.append(k)
+
+    ret = { 'long': [], 'short': []}
+    gap = data_list[sorted_data[len(sorted_data)-1]] - data_list[sorted_data[0]]
+
+    if arb_chance <= gap and gap <= allowed:
+        ret['long'].append(sorted_data[len(sorted_data)-1])
+        ret['short'].append(sorted_data[0])
+    else:
+        return None
     return ret
--- arbit.py
+++ arbit.py
@@ -4,11 +4,15 @@ import sys
 
 sys.path.append('./Screening')
 sys.path.append('./Parse')
+sys.path.append('./general_lib')
 
 import DataGetNikkei as gdata
 import Arbitrage as arb
 
-DEMO_DIR = './datas/demo_price/'
+#DEMO_DIR = './datas/demo_price/'
+DEMO_DIR = './datas/demo_price2/'
+ALLOWED_DEVIATION = 10.0
+ARBITRAGE_NETCHANGE = 5.0
 
 def main():
     f = open('./input/arb_convenience.txt','r')
@@ -16,17 +20,24 @@ def main():
     for i in f.readlines():
         symbol_list.append(i.strip())
     f.close()
+    res = {}
 
     for i in symbol_list:
-        print(i)
         f= open(DEMO_DIR + i, 'r')
         changes=[]
         for i2 in f.readlines():
             changes.append(float(i2.strip()))
-        print(changes)
-        print(arb.calc_total_changed(changes))
-    res = {}
+        res.update({ i: arb.calc_total_changed(changes)})
 
+    data_list = []
+    for i in res.keys():
+        data_list.append(res[i])
+    diviation = arb.calc_standard_deviation(data_list)
+
+    #print(diviation)
+    print(res)
+    chance = arb.check_arbitrage_chance(res, ALLOWED_DEVIATION, ARBITRAGE_NETCHANGE)
+    print(chance)
 
 
 
